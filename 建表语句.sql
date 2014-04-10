43326f6dace893ec89e00dc4bd90da978b4981ab
yubaoquan <721731428@qq.com>
2014-04-10 08:40:50 +0800

    4.5.1
    1.整理建表语句

--- 建表语句.sql
+++ 建表语句.sql
@@ -1,102 +1,28 @@
-﻿create table user (username varchar(50) primary key, 
-								password varchar(50) not null);
-
-create table mail (id int primary key, 
-								sender varchar(50), 
-								addressee varchar(50), 
-								subject varchar(100) , 
-								content text, 
-								senddate timestamp);
-
-create table attachment (id int primary key, 
-											position varchar(100), 
-											mail_id int, 
-											serial_number int, 
-											offset varchar(50), 
-											addressee_name varchar(50));
-											
-alter table mail add constraint sender_fk foreign key (sender)  references user (username);
-alter table mail add constraint addressee_fk foreign key (addressee)  references user (username);
-
-alter table attachment add constraint mail_id_fk foreign key (mail_id)  references mail (id);
-alter table attachment add constraint offset_fk foreign key (offset)  references user (username);
-alter table attachment add constraint addressee_name_fk foreign key (addressee_name)  references user (username);
-
-
-CREATE TABLE `attachment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `position` varchar(100) DEFAULT NULL,
-  `mail_id` int(11) DEFAULT NULL,
-  `offset` int(11) DEFAULT NULL COMMENT '在附件列表中的序号，每个邮件有一个附件列表',
-  `sender_name` varchar(50) DEFAULT NULL,
-  `addressee_name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `mail_id_fk` (`mail_id`),
-  KEY `sender_name_fk` (`sender_name`),
-  KEY `addressee_name_fk` (`addressee_name`),
-  CONSTRAINT `addressee_name_fk` FOREIGN KEY (`addressee_name`) REFERENCES `user` (`username`),
-  CONSTRAINT `mail_id_fk` FOREIGN KEY (`mail_id`) REFERENCES `mail` (`id`),
-  CONSTRAINT `sender_name_fk` FOREIGN KEY (`sender_name`) REFERENCES `user` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `mail` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sender` varchar(50) DEFAULT NULL,
-  `addressee` varchar(50) DEFAULT NULL,
-  `subject` varchar(100) DEFAULT NULL,
-  `content` text,
-  `sendtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `attachment_1` varchar(50) DEFAULT NULL,
-  `attachment_2` varchar(50) DEFAULT NULL,
-  `attachment_3` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `sender_fk` (`sender`),
-  KEY `addressee_fk` (`addressee`),
-  CONSTRAINT `addressee_fk` FOREIGN KEY (`addressee`) REFERENCES `user` (`username`),
-  CONSTRAINT `sender_fk` FOREIGN KEY (`sender`) REFERENCES `user` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
-
 CREATE TABLE `user` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `bysj`.`mail` 
-DROP COLUMN `attachment_3`,
-DROP COLUMN `attachment_2`,
-CHANGE COLUMN `attachment_1` `attachments` TEXT NULL DEFAULT NULL ;
-
-ALTER TABLE `bysj`.`attachment` 
-DROP FOREIGN KEY `sender_name_fk`,
-DROP FOREIGN KEY `addressee_name_fk`;
-ALTER TABLE `bysj`.`attachment` 
-DROP INDEX `sender_name_fk` ;
+CREATE TABLE `mail` (
+  `id` int(11) NOT NULL,
+  `sender` varchar(50) DEFAULT NULL,
+  `addressee` varchar(50) DEFAULT NULL,
+  `subject` varchar(100) DEFAULT NULL,
+  `content` text,
+  `sendtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `attachments` text,
+  PRIMARY KEY (`id`),
+  KEY `sender_fk` (`sender`),
+  KEY `addressee_fk` (`addressee`),
+  CONSTRAINT `sender_fk` FOREIGN KEY (`sender`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `attachment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `location` varchar(100) DEFAULT NULL,
   `mail_id` int(11) DEFAULT NULL,
   `offset` int(11) DEFAULT NULL COMMENT '在附件列表中的序号，每个邮件有一个附件列表',
-  `sender_name` varchar(50) DEFAULT NULL,
-  `addressee_name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `mail_id_fk` (`mail_id`),
-  KEY `addressee_name_fk` (`addressee_name`),
-  CONSTRAINT `mail_id_fk` FOREIGN KEY (`mail_id`) REFERENCES `mail` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-ALTER TABLE `bysj`.`attachment` 
-DROP COLUMN `addressee_name`,
-DROP COLUMN `sender_name`,
-DROP INDEX `addressee_name_fk` ;
-
-ALTER TABLE `bysj`.`attachment` 
-DROP FOREIGN KEY `mail_id_fk`;
-
-ALTER TABLE `bysj`.`mail` 
-CHANGE COLUMN `id` `id` INT(11) NOT NULL ;
-
+  KEY `mail_id_fk` (`mail_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
